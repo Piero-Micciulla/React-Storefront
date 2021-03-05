66102f231cfcbc912f0e415d01681a538f156c8a
Piero Micciulla <micciullapiero@gmail.com>
2021-03-05 14:18:34 +0100

    stored products in localStorage

--- src/components/StoreFront.js
+++ src/components/StoreFront.js
@@ -1,13 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Product from "./Product.js";
 import AddProductForm from "./AddProductForm";
 import ProductsList from "./ProductsList";
 
 export default function StoreFront() {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState(() => {
+    const savedProducts = localStorage.getItem("products");
+    if (savedProducts) {
+      return JSON.parse(savedProducts);
+    } else {
+      return [];
+    }
+  });
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [validation, setValidation] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("products", JSON.stringify(products));
+  }, [products]);
+
+  useEffect(() => {
+    if (products.length === 0) {
+      document.title = "No products";
+    } else if (products.length === 1) {
+      document.title = "1 product";
+    } else {
+      document.title = `${products.length} products`;
+    }
+  }, [products]);
 
   function handleFormSubmit(e) {
     e.preventDefault();
